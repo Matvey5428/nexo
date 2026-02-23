fc417cab133bf1764cde8b75134f401a5cb818a2
Matvey <matveynikit@gmail.com>
2026-02-23 16:56:06 +0200

    Добавлена страница exchange.html обновлекнный index.html и бот

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -1,22 +1,29 @@
+import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
-from aiogram.utils import executor
 
 # Токен твоего бота
-BOT_TOKEN = "8613625963:AAFBGozcjBMJ6OyCbTC2GBvMxPTrFwfGYHo"
+BOT_TOKEN = "8613625963:AAEbL9d5GQNmTy1xXgjh-dJq0IJ2m8aJ8SA"
 
 # Ссылка на твой сайт
 WEB_APP_URL = "https://matvey5428.github.io/nexo/"
 
+# Создаём объекты бота и диспетчера
 bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher()
 
-@dp.message_handler(commands=["start"])
-async def start(message: types.Message):
-    web_app = WebAppInfo(url=WEB_APP_URL)
-    button = InlineKeyboardButton(text="Открыть мини-приложение", web_app=web_app)
-    keyboard = InlineKeyboardMarkup().add(button)
-    await message.answer("Привет! Нажми на кнопку ниже, чтобы открыть приложение:", reply_markup=keyboard)
+# Хэндлер для команды /start
+@dp.message()
+async def handle_start(message: types.Message):
+    if message.text == "start":
+        web_app = WebAppInfo(url=WEB_APP_URL)
+        button = InlineKeyboardButton(text="Торговать", web_app=web_app)
+        keyboard = InlineKeyboardMarkup().add(button)
+        await message.answer("нажмите торговать", reply_markup=keyboard)
+
+# Основная функция запуска бота
+async def main():
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+    asyncio.run(main())
